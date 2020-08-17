87d28f1c11860319f4b8fc97f3e9af02b59667f4
biningo <biningo@yeah.net>
2020-08-17 10:56:26 +0800

    profix查询

---
+++ src/main/java/SmsLogDemo/profix查询.java
@@ -0,0 +1,45 @@
+package SmsLogDemo;
+
+import Client.MyClient;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+
+/***
+ *@Author icepan
+ *@Date 2020/8/17 上午10:23
+ *@Description
+ *
+ ***/
+
+
+public class profix查询 {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient client = MyClient.get();
+
+        SearchRequest request = new SearchRequest("sms-logs");
+
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+
+        builder.query(QueryBuilders.prefixQuery("province","上"));
+
+        request.source(builder);
+
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        for (SearchHit hit : response.getHits()) {
+            System.out.println(hit.getSourceAsMap());
+        }
+
+
+
+
+        client.close();
+    }
+}
